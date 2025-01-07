f2c6b88c5cf93dae97469b472ef08120e66f5899
Lucas Detlor <lucasjd0112@gmail.com>
2025-01-07 11:03:58 -0800

    vfinal

--- task1.py
+++ task1.py
@@ -1,6 +1,20 @@
 def A():
-
+    print('Hello')
 def B():
-
-
+    print('How are you')
 def C():
+    print('Hi')
+
+x = input('Enter A,B, or C: ')
+
+if x == 'A':
+    A()
+elif x == 'B':
+    B()
+elif x == 'C':
+    C()
+else:
+    print('You did not input a correct option.')
+
+
+#done
--- task2.py
+++ task2.py
@@ -8,3 +8,46 @@ displays instructions and how to play
 game()
 plays the games
 """
+
+import random, os
+
+
+def title():
+    os.system('cls')
+    print('Welcome to Number Guesser')
+    print('=========================')
+    print("Guess a number from 1-100.")
+    print('You will be told if your answer is too low or too high.')
+    input('Press enter when you are ready.')
+    os.system('cls')
+
+def game():
+    answer = random.randrange(1,100)
+    guess = 0
+    attempt = 0
+    win = False
+    while win == False:
+        if guess != 0:
+            print(f'Your previous guess was {guess}.')
+        guess = int(input("Enter your guess: "))
+        if guess < answer:
+            os.system('cls')
+            print('You guessed too low.')
+        if guess > answer:
+            os.system('cls') 
+            print('You guessed too high.')
+        if guess == answer:
+            os.system('cls')
+            win = True
+        attempt += 1
+    if attempt == 1:
+        print('Wow! You guessed it first try!')
+        print(f'The number was {answer}.')
+    elif attempt > 1:
+        print(f'Nice! It took {attempt} guesses.')
+        print(f'The number was {answer}.')
+
+title()
+game()
+
+#done
